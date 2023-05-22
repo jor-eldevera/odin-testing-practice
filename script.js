8a187595b4335326395e08bf9edd2418b101b1ca
jor-eldevera <jorell.devera@gmail.com>
2023-05-22 15:49:04 -0700

    Write function skeletons

---
+++ script.js
@@ -0,0 +1,17 @@
+function capitalize(string) {
+    
+}
+
+function reverseString(string) {
+    
+}
+
+let calculator = {};
+
+function caesarCipher(string) {
+    
+}
+
+function analyzeArray(array) {
+    
+}
