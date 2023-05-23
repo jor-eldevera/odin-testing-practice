e58f0b3fb42528357573978a11d0a135e93cdd20
jor-eldevera <jorell.devera@gmail.com>
2023-05-22 17:41:40 -0700

    Write reverseString and calculator

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ function capitalize(string) {
 }
 
 function reverseString(string) {
-    
+    if (string === "") throw new Error("capitalize: empty string passed");
+    if (typeof string !== "string") throw new Error("capitalize: non-string input");
+
+    let newString = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string.charAt(i);
+    }
+
+    return newString;
 }
 
-let calculator = {};
+let calculator = {
+    add: function (x, y) {
+        if (x === undefined || y === undefined) throw new Error("add: lacking parameters");
+        return parseFloat(x) + parseFloat(y);
+    },
+    subtract: function (x, y) {
+        if (x === undefined || y === undefined) throw new Error("add: lacking parameters");
+        return parseFloat(x) - parseFloat(y);
+    },
+    multiply: function (x, y) {
+        if (x === undefined || y === undefined) throw new Error("add: lacking parameters");
+        return parseFloat(x) * parseFloat(y);
+    },
+    divide: function (x, y) {
+        if (x === undefined || y === undefined) throw new Error("add: lacking parameters");
+        return parseFloat(x) / parseFloat(y);
+    },
+};
 
 function caesarCipher(string) {
     
