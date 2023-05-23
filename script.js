7cca628ae4dd0fefa513a07b22f522abe3610a8b
jor-eldevera <jorell.devera@gmail.com>
2023-05-22 17:24:20 -0700

    Write capitalize

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function capitalize(string) {
-    
+    if (string === "") throw new Error("capitalize: empty string passed");
+    if (typeof string !== "string") throw new Error("capitalize: non-string input");
+    return string.charAt(0).toUpperCase() + string.substring(1);
 }
 
 function reverseString(string) {
@@ -14,4 +16,12 @@ function caesarCipher(string) {
 
 function analyzeArray(array) {
     
+}
+
+module.exports = {
+    capitalize,
+    reverseString,
+    calculator,
+    caesarCipher,
+    analyzeArray
 }
